e69b633b62da983c411aa3f9d0a329123f603ad4
Vitor Ferreira <vitorferreiravf2@gmail.com>
2022-05-16 18:16:46 -0300

    up2

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -5,17 +5,8 @@ export default function Header() {
     <HeaderContainer>
       <HeaderContent>
         <img src="/images/caminhao.png" alt="correios" />
-        {/* <nav>
-          <Link href="/">
-            <a>Home</a>
-          </Link>
 
-          <Link href="/results">
-            <a>results</a>
-          </Link>
-        </nav> */}
         <img className="img-git" src="/images/git2.png"></img>
-        {/* <button>Codigo de fonte</button> */}
       </HeaderContent>
     </HeaderContainer>
   )
